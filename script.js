6fc4a5a3e74f66566e0ed1eee7f59c1cf9cbd41c
Hinglerio Carlos <84482831+candez23@users.noreply.github.com>
2024-11-27 22:27:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Validación del formulario de contacto
+const formulario = document.getElementById("form-contacto");
+// Botón para abrir/cerrar el menú
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const btnArriba = document.getElementById('btn-arriba');
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        btnArriba.style.display = 'block';
+    } else {
+        btnArriba.style.display = 'none';
+    }
+});
+
+btnArriba.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("menu-open");
+});
+
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensaje = document.getElementById("mensaje").value.trim();
+
+    if (nombre === "" || email === "" || mensaje === "") {
+        alert("Por favor, completa todos los campos.");
+        return;
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Por favor, ingresa un correo electrónico válido.");
+        return;
+    }
+
+    alert(`Gracias por tu mensaje, ${nombre}. ¡Me pondré en contacto contigo pronto!`);
+    formulario.reset(); // Limpia los campos del formulario
+});
+
